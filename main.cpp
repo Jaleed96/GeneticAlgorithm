c7db4bfba5276da6293b2b1759a5c171b64d1cfd
Jaleed Ur Rehman <jaleed12@me.com>
2019-03-17 11:57:05 -0700

    added final fitnessrating

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+#include "tour.hpp"
+#include <sstream>
+#include <vector>
+#include "city.hpp"
+#include <cmath>
+
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<city> list;
+    city london{"London", 0.0, 120.0};
+    city manchester{"London", 20.0, 180.0};
+    city liverpool{"London", 10.0, 60.0};
+    list.push_back(london);
+    list.push_back(manchester);
+    list.push_back(liverpool);
+
+    tour basicTour{list};
+    cout<<basicTour.getFitnessRating()<<endl;
+
     return 0;
 }
--- tour.cpp
+++ tour.cpp
@@ -6,12 +6,18 @@
 #include <sstream>
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 tour::tour(std::vector<city> listOfCities) : cityTour(listOfCities) {
     // TODO: Calculate fitness rating here
-
+    double xTravelled = 0.0, yTravelled = 0.0;
+    for (int i = 1; i<cityTour.size(); i++) {
+        xTravelled = xTravelled+abs(cityTour[i-1].getX()-cityTour[i].getX());
+        yTravelled = yTravelled+abs(cityTour[i-1].getY()-cityTour[i].getY());
+    }
+    fitnessRating = 1/(10*(xTravelled+yTravelled));
 }
 std::vector<city> tour::getTour() const { return cityTour; }
 
