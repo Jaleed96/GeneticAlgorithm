2c941a5feae365f1ec5323849364e753089e15fa
Jaleed Ur Rehman <jaleed12@me.com>
2019-03-17 11:08:44 -0700

    updated bounds

--- city.cpp
+++ city.cpp
@@ -4,10 +4,22 @@
 
 #include "city.hpp"
 #include <sstream>
+#include <iostream>
 
 using namespace std;
 
-city::city(std::string name, double xCoord, double yCoord) : name(name), x(xCoord), y(yCoord) {}
+city::city(std::string name, double xCoord, double yCoord) : name(name) {
+    if (xCoord>1000.0 || xCoord<0.0) {
+        cerr << "Please make sure X-Coordinate is between 0.0 and 1000.0";
+        throw "CoordinateOutOfBoundsException";
+    }
+    if (yCoord>1000.0 || yCoord<0.0) {
+        cerr << "Please make sure Y-Coordinate is between 0.0 and 1000.0";
+        throw "CoordinateOutOfBoundsException";
+    }
+    this->x = xCoord;
+    this->y = yCoord;
+}
 
 std::string city::getName() const { return this->name; }
 
